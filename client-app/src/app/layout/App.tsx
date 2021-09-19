0cb95ecbbe13e060431bcb9f62ab0af08c3b5bae
Jukka Nokelainen <jukka.nokelainen@gmail.com>
2021-09-19 19:19:04 +0300

    Folder structure for react app

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios'
 import { Header, List } from 'semantic-ui-react';
 
@@ -8,8 +6,7 @@ function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
-      console.log(response);
+    axios.get('http://localhost:5000/api/activities').then(response => {;
       setActivities(response.data);
     })
   }, []);
