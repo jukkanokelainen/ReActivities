a3b58b47bb20ba2880f71fff5dee8147bdaa8522
Jukka Nokelainen <jukka.nokelainen@gmail.com>
2021-09-19 19:36:36 +0300

    interface for activity  and type safety in App.tsx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {;
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {;
       setActivities(response.data);
     })
   }, []);
@@ -16,7 +17,7 @@ function App() {
       <Header as='h2' icon='users' content='Reactivities'/>
       
       <List>
-          {activities.map((item: any) => (
+          {activities.map((item) => (
             <List.Item key={item.id}>
               {item.title}
               </List.Item>
