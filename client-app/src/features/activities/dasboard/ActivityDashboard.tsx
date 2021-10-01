6186be21e4564bda39c7fc1e761c8a10d29b6563
Jukka Nokelainen <jukka.nokelainen@gmail.com>
2021-10-01 16:07:44 +0300

    Add ActivityForm (init)

--- client-app/src/features/activities/dasboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dasboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
 import ActivityDetails from '../details/ActivityDetails'
+import ActivityForm from '../form/ActivityForm'
 import ActivityList from './ActivityList'
 
 interface Props { activities: Activity[]}
@@ -14,6 +15,7 @@ function ActivityDashboard({activities} : Props) {
             <Grid.Column width='6'>
                 {activities[0] &&
                 <ActivityDetails activity={activities[0]}/>}
+                <ActivityForm />
             </Grid.Column>
         </Grid>
     )
