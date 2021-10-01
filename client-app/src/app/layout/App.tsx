7703ce8e9e7c0576e225718629de605ccf9b39ea
Jukka Nokelainen <jukka.nokelainen@gmail.com>
2021-10-01 12:25:58 +0300

    Adding a navbar & activitylist

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import axios from 'axios'
-import { Header, List } from 'semantic-ui-react';
+import { Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dasboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,18 +15,12 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities'/>
-      
-      <List>
-          {activities.map((item) => (
-            <List.Item key={item.id}>
-              {item.title}
-              </List.Item>
-          ))}
-      </List>
-
-    </div>
+    <Fragment>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard activities={activities} />
+      </Container>   
+    </Fragment>
   );
 }
 
