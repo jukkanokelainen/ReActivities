7fdc80718e4c2a58aab429d612f21f1f4661e283
Jukka Nokelainen <jukka.nokelainen@gmail.com>
2021-10-23 10:18:35 +0300

    Make app as observer for observable rendering demo

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,16 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
 
 export default class ActivityStore{
     title = 'Hello from MobX';
 
     constructor() {
         makeObservable(this, {
-            title: observable
+            title: observable,
+            setTitle: action
         });
+    }
+
+    setTitle= () => {
+        this.title = this.title + '!';
     }
 }
